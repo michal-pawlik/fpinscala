fbd9f1ccad1788cab85872fe26d04298194e7e6a
dhirajhimani <hdhiraj@thoughtworks.com>
2018-03-29 13:00:16 +0530

    List module done

--- PracticeCode/src/scala/fpinscala/datastructures/List.scala
+++ PracticeCode/src/scala/fpinscala/datastructures/List.scala
@@ -144,4 +144,71 @@ object List {
     foldRight(l, Nil: List[String])((h, t) => Cons(h.toString, t))
   }
 
+  def map[A,B](l: List[A])(f: A => B): List[B] = {
+    foldRight(l, Nil: List[B])((h,acc) => Cons(f(h), acc))
+  }
+
+  def filter[A](l: List[A])(f: A => Boolean): List[A] = {
+    foldRight(l, Nil: List[A])((h,acc) => if(f(h)) Cons(h, acc) else acc)
+  }
+
+  def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = {
+    concat(map(l)(f))
+  }
+
+  def flatMap2[A,B](l: List[A])(f: A => List[B]): List[B] = {
+    foldRight(l, Nil: List[B])((h,acc) => append(f(h), acc))
+  }
+
+  def flatMap3[A,B](l: List[A])(f: A => List[B]): List[B] = {
+    concat(foldRight(l, Nil: List[List[B]])((h,t) => Cons(f(h), t)))
+  }
+
+  def filterViaFlatMap[A](l: List[A])(f: A => Boolean): List[A] = {
+    flatMap(l)(a => if (f(a)) List(a) else Nil)
+  }
+
+  def addPairwise(a: List[Int], b: List[Int]): List[Int] = (a,b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2,t2)) => Cons(h1+h2, addPairwise(t1,t2))
+
+  }
+
+  def addPairwise2(a: List[Int], b: List[Int]): List[Int] = (a,b) match {
+    case (Nil, Nil) => Nil
+    case (Cons(h1, t1), Nil) => Cons(h1, t1)
+    case (Nil, Cons(h2,t2)) => Cons(h2,t2)
+    case (Cons(h1, t1), Cons(h2,t2)) => Cons(h1+h2, addPairwise2(t1,t2))
+  }
+
+  def zipWith[A,B,C](a: List[A], b: List[B])(f: (A,B) => C): List[C] = (a,b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2,t2)) => Cons(f(h1,h2), zipWith(t1,t2)(f))
+  }
+
+
+  @annotation.tailrec
+  def startsWith[A](l: List[A], prefix: List[A]): Boolean = (l,prefix) match {
+    case (_,Nil) => true
+    case (Cons(h,t),Cons(h2,t2)) if h == h2 => startsWith(t, t2)
+    case _ => false
+  }
+
+  @annotation.tailrec
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = sup match {
+    case Nil => sub == Nil
+    case _ if startsWith(sup, sub) => true
+    case Cons(h,t) => hasSubsequence(t, sub)
+  }
+
+  @annotation.tailrec
+  def hasSubsequence2[A](l: List[A], prefix: List[A]): Boolean = (l,prefix) match {
+    case (_, Nil) => true
+    case (Cons(h1,t1), Cons(h2,t2)) => if (h1 == h2) hasSubsequence2(t1,t2) else hasSubsequence2(t1,prefix)
+    case (Nil, p) => Nil == p
+  }
+
+
 }
