508b8d98e1498ea2d530ada9b64cf4b2a6864062
dhirajhimani <hdhiraj@thoughtworks.com>
2018-04-12 07:16:46 +0530

    Working on getting to flatmap where its usage comes

--- PracticeCode/src/scala/fpinscala/state/State.scala
+++ PracticeCode/src/scala/fpinscala/state/State.scala
@@ -77,4 +77,34 @@ object RNG {
   val _double: Rand[Double] = {
       map(rng => nonNegativeInt(rng))(b => b / Int.MaxValue.toDouble + 1)
   }
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    rng => {
+      val (a, rng2) = ra(rng)
+      val (b, rng3) = rb(rng2)
+      (f(a, b), rng3)
+    }
+  }
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
+    map2(ra, rb)((_, _))
+
+  val randIntDouble: Rand[(Int, Double)] =
+    both(int, double)
+
+  val randDoubleInt: Rand[(Double, Int)] =
+    both(double, int)
+
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    fs.foldRight(unit(List[A]()))((f, acc) => map2(f,acc)(_ :: _))
+  }
+
+  def _ints(count: Int): Rand[List[Int]] = {
+    sequence(List.fill(count)(int))
+  }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    map(nonNegativeInt)(_ % n)
+  }
 }
