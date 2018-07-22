5e62183bc34b1c705a27ce58d508889a3ed08ae4
Dhiraj Himani <dhrjkmr538@gmail.com>
2018-07-22 17:17:56 +0530

    done with ch2 getting started and its test

--- PracticeCode/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ PracticeCode/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -1,5 +1,52 @@
 package scala.fpinscala.gettingstarted
 
+// On Page 21
+
 object GettingStarted {
+
   def printHello = "Hello"
+
+  def fibonacci(n: Int): Int = {
+    n match {
+      case _ if n < 1 => 0
+      case 1 => 0
+      case 2 => 1
+      case _ => fibonacci(n-1) + fibonacci(n-2)
+    }
+  }
+
+  def isSorted[A](arr: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    var sorted = true
+    for(i <- 0 until arr.size - 1) {
+      if (!(sorted && ordered(arr(i), arr(i+1)))) {
+        sorted = false
+      }
+    }
+    sorted
+  }
+
+  def isSorted2[A](arr: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    def go(n:Int): Boolean = {
+      n match {
+        case _ if n >= arr.size - 1 => true
+        case _ if !ordered(arr(n), arr(n+1)) => false
+        case _ => go(n+1)
+      }
+    }
+    go(0)
+  }
+
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) = {
+    a:A => b:B => f(a,b)
+  }
+
+  def unCurry[A,B,C](f: A => B => C): (A,B) => C = {
+    (a:A,b:B) => f(a)(b)
+  }
+
+  def compose[A,B,C](doubleMe: A => B, squareMe: B => C): A => C = {
+    a:A => squareMe(doubleMe(a))
+  }
+
+
 }
--- PracticeCode/src/test/scala/fpinscala/gettingstarted/GettingStartedTest.scala
+++ PracticeCode/src/test/scala/fpinscala/gettingstarted/GettingStartedTest.scala
@@ -6,12 +6,94 @@ import scala.fpinscala.gettingstarted.GettingStarted
 
 class GettingStartedTest extends WordSpec {
 
-    ".printHello" when {
-      "return Hello" in {
-        assertResult("Hello"){
-          GettingStarted.printHello
-        }
+  ".printHello" when {
+    "will return Hello" in {
+      assertResult("Hello") {
+        GettingStarted.printHello
       }
     }
+  }
+
+  ".fibonacci" when {
+    "will return fibonacci by position" in {
+      "1st fibanacci is 0"
+      assertResult(0) {
+        GettingStarted.fibonacci(1)
+      }
+      "2nd fibanacci is 1"
+      assertResult(1) {
+        GettingStarted.fibonacci(2)
+      }
+      "3rd fibanacci is 1"
+      assertResult(1) {
+        GettingStarted.fibonacci(3)
+      }
+      "4th fibanacci is 2"
+      assertResult(2) {
+        GettingStarted.fibonacci(4)
+      }
+      "5th fibanacci is 3"
+      assertResult(3) {
+        GettingStarted.fibonacci(5)
+      }
+      "6th fibanacci is 5"
+      assertResult(5) {
+        GettingStarted.fibonacci(6)
+      }
+    }
+  }
+
+  ".isSorted" when {
+    "will return accordingly if its Sorted List" in {
+      assertResult(true) {
+        GettingStarted.isSorted[Int](Array(1,2,3,4), (x,y) => x < y)
+      }
+      assertResult(false) {
+        GettingStarted.isSorted[Int](Array(1,2,4,3), (x,y) => x < y)
+      }
+      assertResult(true) {
+        GettingStarted.isSorted[String](Array("a","b","c","d"), (x,y) => x.compareTo(y) < 0)
+      }
+      assertResult(false) {
+        GettingStarted.isSorted[String](Array("b","a","c","d"), (x,y) => x.compareTo(y) < 0)
+      }
+    }
+  }
+
+  def div(a:Int, b:Int):Int = {
+    a/b
+  }
+
+
+  def div2(a:Int): Int => Int = {
+    b:Int => a/b
+  }
+
+  "curry and uncurry" should {
+    "return the same result" in {
+      assertResult(5) {
+        GettingStarted.curry(div)(11)(2)
+      }
+      assertResult(5) {
+        GettingStarted.unCurry(div2)(11,2)
+      }
+    }
+  }
+
+  def doubleMe(a:Int): Int= {
+    a * 2
+  }
+
+  def squareMe(a:Int):Int = {
+    Math.pow(a,2).toInt
+  }
+
+  ".compose" should {
+    "Compose two function into One" in {
+      assertResult(100) {
+        GettingStarted.compose(doubleMe, squareMe)(5)
+      }
+    }
+  }
 
 }
